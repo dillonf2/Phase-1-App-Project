e070722788ec448f71536d5965cee8641b1310ba
Frank Dillon <dillonf2@outlook.com>
2023-04-03 14:35:11 -0400

    added functional obj to list function that appends an obj to the DOM

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 testObj = {
-    title: "This is test object 1",
+    title: "This is Test Object 1",
     one :1,
     two: 2,
     three: 3
 }
-// document.addEventListener(`DOMContentLoaded`, function (){
-//     document.querySelector(`#divObj`).append(testObj)
-//     document.querySelector(`#titleId`).append(testObj[`title`])
-// })
+
+function objToList(obj){
+    for (const key in obj){
+        if(key!==`title`){
+        newLi=document.createElement(`li`)
+        newLi.textContent=`${key}: ${obj[key]}`
+        document.querySelector(`#testUl`).append(newLi)
+        }
+    }
+}
+
+
+document.addEventListener(`DOMContentLoaded`, function (){
+    document.querySelector(`#testUl`).append(testObj[`title`])
+    objToList(testObj)
+})
