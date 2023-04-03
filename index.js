76067f24ea638fdfe5d6ffb7e874c7aadbd13088
Frank Dillon <dillonf2@outlook.com>
2023-04-03 14:17:55 -0400

    init test Obj and lay basic HTML foundation

---
+++ index.js
@@ -0,0 +1,10 @@
+testObj = {
+    title: "This is test object 1",
+    one :1,
+    two: 2,
+    three: 3
+}
+// document.addEventListener(`DOMContentLoaded`, function (){
+//     document.querySelector(`#divObj`).append(testObj)
+//     document.querySelector(`#titleId`).append(testObj[`title`])
+// })
