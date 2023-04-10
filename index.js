81ecafadce1310fccaa6f0a0a4b65e8062bafe3f
Frank Dillon <dillonf2@outlook.com>
2023-04-10 17:26:25 -0400

    fixed formatting of volume and market cap as well by adding commas and $

--- index.js
+++ index.js
@@ -12,24 +12,25 @@ document.addEventListener(`DOMContentLoaded`, function(){
     .then((res)=>{return res.json()})
     .then((data)=>{
         console.log(data)
-        let dataObj=data
         for(let i=0; i<10; i++){
             const currentAsset=document.createElement(`li`)
             currentAsset.setAttribute(`id`,`listItems`)
             // currentAsset.textContent=`${dataObj[`data`][i][`name`]} // Current Price : $${Number(dataObj[`data`][i][`priceUsd`]).toFixed(2).toLocaleString("en-US")} //  24hr Volume: $${(Number(dataObj[`data`][i][`volumeUsd24Hr`]).toFixed(2)).toLocaleString("en-US")} // Market Cap: $${(Number(dataObj[`data`][i][`marketCapUsd`]).toFixed(2)).toLocaleString("en-US")}`
-            const price=dataObj.data[i].priceUsd
+            const price=data.data[i].priceUsd
+            const volume=data.data[i].volumeUsd24Hr
+            const marketCap=data.data[i].marketCapUsd
             const convertedPrice="$" + parseFloat(price).toFixed(2).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
-            currentAsset.textContent=`${dataObj[`data`][i][`name`]} // Current Price : ${convertedPrice} //  24hr Volume: $${(Number(dataObj[`data`][i][`volumeUsd24Hr`]).toFixed(2)).toLocaleString("en-US")} // Market Cap: $${(Number(dataObj[`data`][i][`marketCapUsd`]).toFixed(2)).toLocaleString("en-US")}`
+            const convertedVolume="$" + parseFloat(volume).toFixed(2).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+            const convertedMarketCap="$" + parseFloat(marketCap).toFixed(2).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+            currentAsset.textContent=`${data.data[i][`name`]} // Current Price : ${convertedPrice} //  24hr Volume: ${convertedVolume} // Market Cap: $${convertedMarketCap}`
             document.querySelector(`#crypto-ol`).append(currentAsset)
         }
     })
 
 })
-document.addEventListener(`DOMContentLoaded`, ()=>{
-    document.querySelectorAll(`li`).forEach(()=>{console.log(`bitcoin`)})
-})
+
 // const e=document.querySelectorAll(`li`).forEach(function(){console.log(`bitcoin`)})
 // console.log(e)
 // amt = "$" + amt.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-amt= "$" + parseFloat(x).toFixed(2).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
-currentAsset.textContent=`${dataObj[`data`][i][`name`]} // Current Price : $${Number(dataObj[`data`][i][`priceUsd`]).toFixed(2).toLocaleString("en-US")} //  24hr Volume: $${(Number(dataObj[`data`][i][`volumeUsd24Hr`]).toFixed(2)).toLocaleString("en-US")} // Market Cap: $${(Number(dataObj[`data`][i][`marketCapUsd`]).toFixed(2)).toLocaleString("en-US")}`
+// amt= "$" + parseFloat(x).toFixed(2).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+// currentAsset.textContent=`${dataObj[`data`][i][`name`]} // Current Price : $${Number(dataObj[`data`][i][`priceUsd`]).toFixed(2).toLocaleString("en-US")} //  24hr Volume: $${(Number(dataObj[`data`][i][`volumeUsd24Hr`]).toFixed(2)).toLocaleString("en-US")} // Market Cap: $${(Number(dataObj[`data`][i][`marketCapUsd`]).toFixed(2)).toLocaleString("en-US")}`
